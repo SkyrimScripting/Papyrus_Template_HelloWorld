8a17e7a33f1bc34c13dbb15024733bbe835615e0
Mrowr Purr <mrowr.purr@gmail.com>
2023-01-05 20:54:30 -0800

    Simpler Compiler.bat - with UI error messages

--- Compile.bat
+++ Compile.bat
@@ -28,75 +28,55 @@ set OUTPUT_FOLDER=Scripts
 :: Compile.bat script code below
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
+:: For newlines in PowerShell commands
+(set \n=^
+%=Do not remove this line=%
+)
+
 :: Check for pyro.exe
 where /q pyro
 if %ERRORLEVEL% == 0 (
     echo ^[FOUND] pyro
-    echo ^Searching for .ppj Papyrus Project Files
+    echo ^[SEARCH] .ppj Papyrus Project Files
     if exist *.ppj (
-        echo ^Compiling project using pyro
+        echo ^[COMPILE] Compiling project using pyro
         for /r %%a in (*.ppj) do echo^ [FOUND] "%%a"
         for /r %%a in (*.ppj) do pyro -i "%%a"
         goto :done
     ) else (
-        echo ^No .ppj Papuyrus Project Files found
-        echo ^Falling back to PapyrusCompiler.exe...
+        echo ^[NOT FOUND] No .ppj Papuyrus Project Files found
+        echo ^[INFO] Falling back to PapyrusCompiler.exe...
     )
 ) else (
     echo ^[NOT FOUND] pyro
-    echo ^Falling back to PapyrusCompiler.exe...
+    echo ^[INFO] Falling back to PapyrusCompiler.exe...
 )
-
-goto :done
 
 :: Check for PapyrusCompiler.exe
 where /q PapyrusCompiler
 if %ERRORLEVEL% == 0 (
     echo ^[FOUND] PapyrusCompiler.exe
-) else (
-    echo ^[NOT FOUND] PapyrusCompiler.exe
-    echo ^Searching for PapyrusCompiler.exe...
 )
 
 if not exist "%SKYRIM_FOLDER%" (
-    echo ^Could not find Skyrim Folder "%SKYRIM_FOLDER%"
-    goto :error
+    echo ^[NOT FOUND] Skyrim Folder "%SKYRIM_FOLDER%"
+    set ERROR_MSG=^[ERROR] Could not find Skyrim folder
+    set ERROR_MSG=^!ERROR_MSG!`n`nExpected path:
+    set ERROR_MSG=^!ERROR_MSG!`n"%SKYRIM_FOLDER%"
+    goto :error_msg
 )
 
-echo here
+echo ^[SEARCH] PapyrusCompiler.exe
 set PAPYRUS_COMPILER=%SKYRIM_FOLDER%\Papyrus Compiler\PapyrusCompiler.exe
-echo there
 if exist "%PAPYRUS_COMPILER%" (
-    echo ^[FOUND] PapyrusCompiler.exe
-    echo "%PAPYRUS_COMPILER%"
+    echo ^[FOUND] PapyrusCompiler.exe "%PAPYRUS_COMPILER%"
 ) else (
-    echo ^Could not find Papyrus compiler "%PAPYRUS_COMPILER%"
-    goto :error
+    echo ^[NOT FOUND] PapyrusCompiler.exe
+    set ERROR_MSG=^[ERROR] Could not find Papyrus compiler
+    set ERROR_MSG=^!ERROR_MSG!`n`nExpected path:
+    set ERROR_MSG=^!ERROR_MSG!`n"%PAPYRUS_COMPILER%"
+    goto :error_msg
 )
-
-if not exist "%SKYRIM_FOLDER%\Data\Scripts\Source\TESV_Papyrus_Flags.flg" (
-    if not exist "%SKYRIM_FOLDER%\Data\Source\Scripts\TESV_Papyrus_Flags.flg" (
-        if exist "%SKYRIM_FOLDER%\Data\Scripts.zip" (
-            echo ^Could not find Papyrus scripts in your "%SKYRIM_FOLDER%\Data" directory
-            echo ^[FOUND] Scripts.zip
-            echo ^Extracting to Data folder: "%SKYRIM_FOLDER%\Data\"
-            echo ^powershell Expand-Archive %SKYRIM_FOLDER%\Data\Scripts.zip -DestinationPath %SKYRIM_FOLDER%\Data
-            powershell -Command "& Expand-Archive -Force '%SKYRIM_FOLDER%\Data\Scripts.zip' -DestinationPath '%SKYRIM_FOLDER%\Data'"
-        )
-    )
-)
-
-if not exist "%SKYRIM_FOLDER%\Data\Scripts\Source\TESV_Papyrus_Flags.flg" (
-    if not exist "%SKYRIM_FOLDER%\Data\Source\Scripts\TESV_Papyrus_Flags.flg" (
-        echo ^Could not find Papyrus scripts in your "%SKYRIM_FOLDER%\Data" directory
-        echo ^Please follow README instructions to unzip the Scripts.zip or scripts.rar
-        echo ^[NOT FOUND] Searched "%SKYRIM_FOLDER%\Data\Scripts\Source"
-        echo ^[NOT FOUND] Searched "%SKYRIM_FOLDER%\Data\Source\Scripts"
-        goto :error
-    )
-)
-
-echo ^Compiling project using PapyrusCompiler.exe
 
 set COMPILER_INCLUDES=Scripts\Source
 if exist "%SKYRIM_FOLDER%\Data\Scripts\Source" (
@@ -106,7 +86,14 @@ if exist "%SKYRIM_FOLDER%\Data\Source\Scripts" (
     set COMPILER_INCLUDES=%COMPILER_INCLUDES%;%SKYRIM_FOLDER%\Data\Source\Scripts
 )
 
+echo.
+echo ^[COMPILE] Compiling project using PapyrusCompiler.exe
+echo ^"%PAPYRUS_COMPILER%" %SCRIPTS_FOLDER% -all -f=TESV_Papyrus_Flags.flg -o=%OUTPUT_FOLDER% -i="%COMPILER_INCLUDES%"
+echo.
 "%PAPYRUS_COMPILER%" %SCRIPTS_FOLDER% -all -f=TESV_Papyrus_Flags.flg -o=%OUTPUT_FOLDER% -i="%COMPILER_INCLUDES%"
+
+echo ^Done!
+goto :done
 
 :error_msg
     powershell -c "Add-Type -Assembly System.Windows.Forms; $result = [System.Windows.Forms.MessageBox]::Show(@\"!\n!!ERROR_MSG!!\n!\"@).ToString(); ''"
