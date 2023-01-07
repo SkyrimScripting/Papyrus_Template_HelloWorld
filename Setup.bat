bd7d573a505e6b39c610d94495076997b44d48b3
Mrowr Purr <mrowr.purr@gmail.com>
2023-01-06 20:06:43 -0800

    Workin Workin Workin in progress progress

--- Setup.bat
+++ Setup.bat
@@ -11,6 +11,7 @@ cls
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 set TEMPLATE_NAME=Hello World
+set TEMPLATE_URL=https://github.com/SkyrimScripting/Papyrus_Template_HelloWorld
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Invoke main() method for each supported action
@@ -24,14 +25,6 @@ if "%1" == "personalize" call :personalize
 goto :end
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:: Misc: required variable for creating UI messages with line breaks
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-
-(set \n=^
-%=DO NOT REMOVE THIS LINE - This creates a variable with a newline character=%
-)
-
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Skyrim Folder (containing Creation Kit)
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
@@ -42,10 +35,14 @@ goto :end
 
 :find_skyrim_folder
     if not "%SKYRIM_FOLDER%" == "" goto :end
+    call :find_steam_folder
+    if "%STEAM_FOLDER%" == "" call :error_steam_not_found
     echo lookup
     goto :end
 
 :find_steam_folder
+    if not "%STEAM_FOLDER%" == "" goto :end
+    @REM for /f "tokens=2* skip=2" %%a in ('reg query "HKCU\SOFTWARE\Valve\Steam" /v "SteamPath"') do set STEAM_FOLDER=%%b
     goto :end
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
@@ -96,15 +93,23 @@ goto :end
 :: User Interface
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
+:use_newlines
+(set \n=^
+%=DO NOT REMOVE THIS LINE - This creates a variable with a newline character=%
+)
+    goto :end
+
 :msgbox_ok
     powershell -c "Add-Type -Assembly System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show(\"${env:MSGBOX_TEXT}\", \"${env:MSGBOX_TITLE}\")"
     goto :end
 
 :msgbox_yes_no
+    set MSGBOX_TYPE=YesNo
     set MSGBOX_RESULT=
     for /f "usebackq delims=" %%i in (`
         powershell -c "Add-Type -Assembly System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show(\"${env:MSGBOX_TEXT}\", \"${env:MSGBOX_TITLE}\", \"${env:MSGBOX_TYPE}\")"
     `) do set MSGBOX_RESULT=%%i
+    if "!MSGBOX_RESULT!" == "Yes" call :open_readme_in_browser
     goto :end
 
 :msgbox_input
@@ -114,8 +119,38 @@ goto :end
     `) do set MSGBOX_RESULT=%%i
     goto :end
 
+:msgbox_redirect_readme
+    set MSGBOX_TEXT=^!MSGBOX_TEXT!!\n!!\n!Would you like to view the README for this template?
+    set MSGBOX_TEXT=^!MSGBOX_TEXT!!\n!!\n!Select "Yes" to open it in a browser
+    call :msgbox_yes_no
+    goto :end
+
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Errors
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+:error_steam_not_found
+    call :use_newlines
+    set MSGBOX_TITLE=^Error
+    set MSGBOX_TEXT=^Could not find find Steam directory
+    set MSGBOX_TEXT=^!MSGBOX_TEXT!!\n!!\n!( Is Steam installed? )
+    set MSGBOX_TEXT=^!MSGBOX_TEXT!!\n!!\n!You can fix this error by creating a new Windows environment variable called SKYRIM_FOLDER and setting the value to the full path of your Skyrim or Skyrim Special Edition game directory.
+    call :msgbox_redirect_readme
+    goto :exit
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Misc
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+:open_readme_in_browser
+    start %TEMPLATE_URL%
+    goto :end
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: End of Script
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+:exit
+    exit /b 1
 
 :end
